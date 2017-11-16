d2311d7083deeec69069aba9b479365a417d2afc
Onah-Noelyn <32229778+Onah-Noelyn@users.noreply.github.com>
2017-11-16 16:49:54 +0000

    Edited to include stopping condition

--- model.py
+++ model.py
@@ -6,14 +6,20 @@ import matplotlib.animation
 import agentframework
 import csv
 
+#Parametise the model
+num_of_agents = 10                 #controls how many agents we have
+num_of_iterations = 100            #changes agent cordinates an arbitrary number of times
+neighbourhood = 5                  #guides who agents share resources with
+agents = []                        #create an empty list
+carry_on = True                    #used in stopping the agent movement
+environment = []                    #create an empty list to read file in
 
-#Creating the enviroment using data from a text file
-environment = []                    #create an empty list 
-
+              
+#filling the list:enviroment using data from a text file              
 f = open('in.txt', newline='')      #read the text a line at a time
 dataset = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC) #convert to numbers
 
-for row in dataset:				     #calling the rows a row at a time
+for row in dataset:				     #reading in the rows a row at a time
     rowlist = []                    
     for value in row:
         rowlist.append(value) 
@@ -21,19 +27,14 @@ for row in dataset:				     #calling the rows a row at a time
     
 f.close()                           #always close the worksheet 
 
-         
-#Parametise the models 
-num_of_agents = 10                 #controls how many agents we have
-num_of_iterations = 5            #changes agent cordinates an arbitrary number of times
-neighbourhood = 5                  #guides who agents share resources with
-
+               
 #Make agents
-agents = [] #create an empty list
 for i in range(num_of_agents):  #create a set of agents
     agents.append(agentframework.Agent(environment, agents))
     
 
 #move, eat and share points with neighbours
+#the agent movement is random
 for j in range(num_of_iterations):    
     random.shuffle(agents)        #shuffles each agent list at each iteration
     for item in range(num_of_agents):
@@ -59,32 +60,50 @@ for agent in range(num_of_agents):
         matplotlib.pyplot.scatter(agents[agent].x,agents[agent].y)  
 matplotlib.pyplot.show()
 
-#write new enviroment
+#write the new enviroment to a file
 f3 = open('env.csv', 'w', newline='') 
 writer = csv.writer(f3, delimiter=' ')
 for row in environment:		
     writer.writerow(row)		
-f3.close()
+f3.close
 
 
 #Show Animation of agents eating and moving
+#includes a stopping condition to prevent animation from working in an infinte loop
 fig = matplotlib.pyplot.figure(figsize=(7, 7))
 ax = fig.add_axes([0, 0, 1, 1])
 
+
 def update(frame_work):
     fig.clear()    
+    global carry_on
+    
     for item in range(num_of_agents):
         agents[item].move()
+    
+    if random.random() < 0.1: #this condition decides when the agents stop working
+        carry_on = False
+        print("stopping condition")
+
     for item in range(num_of_agents):
         matplotlib.pyplot.scatter(agents[item].x,agents[item].y)       
     matplotlib.pyplot.imshow(environment)
-animation = matplotlib.animation.FuncAnimation(fig, update, interval=1)
+    
+def gen_function(b = [0]):
+    a = 0
+    global carry_on #Not actually needed as we're not assigning, but clearer
+    while (a < 10) & (carry_on) :
+        yield a			# Returns control and waits next call.
+        a = a + 1
+
+
+animation = matplotlib.animation.FuncAnimation(fig, update, frames=gen_function, repeat=False)
 fig.show()
 
-#transform animatiion into a video
-#to play independent of the code and so attach to a webpage
-animation.save('ABM_animation.mp4', fps=30)
-
+#To transform animation into a video, use next line of code 
+#animation.save('ABM_animation.mp4', fps=30)
+#So as to play independent of the code 
+#this works without error when the code is left to run in an infinite loop i.e. without the stopping condition
 
 #End---------------------------------------------------------------------
 
